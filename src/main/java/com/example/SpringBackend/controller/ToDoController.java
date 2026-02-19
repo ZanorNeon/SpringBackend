7bc2151aca5b233a7e0dc2d5a9e175d494737427
ZanorNeon <raytherayoor@gmail.com>
2026-02-19 17:18:52 +0200

    Added cross origins to localhost 3000.

--- src/main/java/com/example/SpringBackend/controller/ToDoController.java
+++ src/main/java/com/example/SpringBackend/controller/ToDoController.java
@@ -3,6 +3,7 @@ package com.example.SpringBackend.controller;
 import com.example.SpringBackend.model.ToDo;
 import com.example.SpringBackend.service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class ToDoController {
@@ -29,7 +31,7 @@ public class ToDoController {
     }
 
     @GetMapping("/todos/{id}")
-    public ToDo getToDo(@PathVariable long id){
+    public ToDo getToDo(@PathVariable long id) {
         ToDo theToDo = toDoService.findById(id);
         if (theToDo == null) {
             throw new RuntimeException("ToDo id not found - " + id);
